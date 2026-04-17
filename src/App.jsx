a62d812aa59f50a30c1c054f716c5640ca0ce7d5
VGLKSathsara <lanishkakawenn926@gmail.com>
2026-04-17 21:17:58 +0530

    V1.0

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-orange-500">
+      <h1 className="text-white text-4xl font-bold">
+        KadaDev Puja Watti - Ready!
+      </h1>
+    </div>
+  )
+}
+
+export default App
